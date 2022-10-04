9fb885b450a0c0772379c5eb686a925a2ecec627
Patronum129 <diego88rising@gmail.com>
2022-10-04 23:20:31 +0200

    clean the project and fix the bugs

---
+++ Traversed/Application.cpp
@@ -0,0 +1,133 @@
+#include "Application.h"
+
+Application::Application()
+{
+	window = new ModuleWindow(this);
+	input = new ModuleInput(this);
+	renderer3D = new ModuleRenderer3D(this);
+	camera = new ModuleCamera3D(this);
+
+	// The order of calls is very important!
+	// Modules will Init() Start() and Update in this order
+	// They will CleanUp() in reverse order
+
+	// Main Modules
+	AddModule(window);
+	AddModule(camera);
+	AddModule(input);
+	
+	// Scenes
+
+	// Renderer last!
+	AddModule(renderer3D);
+}
+
+Application::~Application()
+{
+	std::list<Module*>::iterator item = list_modules.begin();
+
+	while (item != list_modules.end())
+	{
+		delete item._Ptr->_Myval;
+		item++;
+	}
+	list_modules.clear();
+}
+
+bool Application::Init()
+{
+	bool ret = true;
+
+	std::list<Module*>::iterator item = list_modules.begin();
+
+	// Call Init() in all modules
+
+	while (item != list_modules.end() && ret == true)
+	{
+		ret = item._Ptr->_Myval->Init();
+
+		item++;
+	}
+
+	// After all Init calls we call Start() in all modules
+	LOG("Application Start --------------");
+	item = list_modules.begin();
+
+	while (item != list_modules.end() && ret == true)
+	{
+		ret = item._Ptr->_Myval->Start();
+
+		item++;
+	}
+	
+	ms_timer.Start();
+	return ret;
+}
+
+// ---------------------------------------------
+void Application::PrepareUpdate()
+{
+	dt = (float)ms_timer.Read() / 1000.0f;
+	ms_timer.Start();
+}
+
+// ---------------------------------------------
+void Application::FinishUpdate()
+{
+}
+
+// Call PreUpdate, Update and PostUpdate on all modules
+update_status Application::Update()
+{
+	update_status ret = UPDATE_CONTINUE;
+	PrepareUpdate();
+	
+	std::list<Module*>::iterator item = list_modules.begin();
+	
+	while(item != list_modules.end() && ret == UPDATE_CONTINUE)
+	{
+		ret = item._Ptr->_Myval->PreUpdate(dt);
+
+		item++;
+	}
+
+	item = list_modules.begin();
+
+	while (item != list_modules.end() && ret == UPDATE_CONTINUE)
+	{
+		ret = item._Ptr->_Myval->Update(dt);
+
+		item++;
+	}
+
+	item = list_modules.begin();
+
+	while (item != list_modules.end() && ret == UPDATE_CONTINUE)
+	{
+		ret = item._Ptr->_Myval->PostUpdate(dt);
+
+		item++;
+	}
+
+	FinishUpdate();
+	return ret;
+}
+
+bool Application::CleanUp()
+{
+	bool ret = true;
+	std::list<Module*>::iterator item = list_modules.end();
+	item--;
+
+	while (item != list_modules.begin() && ret == true)
+	{
+		ret = item._Ptr->_Myval->CleanUp();
+		item--;
+	}
+	return ret;
+}
+
+void Application::AddModule(Module* mod)
+{
+	list_modules.push_back(mod);
+}
