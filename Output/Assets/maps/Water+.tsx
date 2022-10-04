70783f454a00c7d7beee47b618dfb194eaf089ba
Oriol <73582891+guerra22@users.noreply.github.com>
2022-10-04 18:50:57 +0200

    no

--- Output/Assets/maps/Water+.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="Water+" tilewidth="16" tileheight="16" tilecount="168" columns="12">
- <image source="Free/Water+.png" width="192" height="224"/>
-</tileset>
