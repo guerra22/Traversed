0aec79a1fba12dab2d135b13982eb088de1d9409
HANGXUE <73582966+Patronum129@users.noreply.github.com>
2022-10-05 10:44:56 +0200

    .

--- Traversed/ModuleUi.h
+++ Traversed/ModuleUi.h
@@ -1,2 +1,34 @@
 #pragma once
+#include "Module.h"
+#include "Globals.h"
+#include "lib/Imgui/imgui.h"
+#include "lib/Imgui/backends/imgui_impl_sdl.h"
+#include "lib/SDL/include/SDL.h"
+#include <vector>
+
+class UI_Item;
+
+class ModuleUI : public Module
+{
+public:
+	ModuleUI(bool start_updated);
+	~ModuleUI();
+
+	bool Init();
+
+	bool Start();
+	update_status PreUpdate(float dt);
+	update_status PostUpdate(float dt);
+	bool CleanUp();
+
+	void RenderImGui();
+
+	bool MainMenu();
+
+	bool CheckDataWindows();
+
+private:
+	
+
+};
 
